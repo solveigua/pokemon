d4bb8049bc54115e9d25effc56655ba7fc8ac80f
solveigua <solveig.aune@live.no>
2023-03-13 23:04:56 +0200

    :lipstick: Add simple styling
    Sort components in divs in app.py, and add css style file with simple styling.

--- app.py
+++ app.py
@@ -8,31 +8,63 @@ from dash import Dash, dcc, html
 data = (
     pd.read_csv("pokemon.csv", index_col="name", on_bad_lines="skip")
 )
-print(data)
+#print(data)
 
-app = Dash(__name__)
+# STYLESHEET config
+
+external_stylesheets = [
+    {
+        "href": (
+            "https://fonts.googleapis.com/css2?"
+            "family=Lato:wght@400;700&display=swap"
+        ),
+        "rel": "stylesheet",
+    },
+]
+
+app = Dash(__name__, external_stylesheets=external_stylesheets)
+app.title = "Pokemon index"
 
 # LAYOUT
 
 app.layout = html.Div(
     children=[
-        html.H1(children="Pokemon index"),
-        html.P(
-            children=(
-                "Find the perfect Pokémon for your battle!"
-            ),
-        ),
-                dcc.Graph(
-            figure={
-                "data": [
-                    {
-                        "x": data["pokedex_number"],
-                        "y": data["speed"],
-                        "type": "bar",
-                    },
-                ],
-                "layout": {"title": "Speed"},
-            },
+        html.Div(
+            children=[
+                html.P(children="👾", className="header-emoji"),
+                html.H1(children="Pokemon index", className="header-title"),
+                html.P(children="Find the perfect Pokémon for your battle!", className="header-description"),
+            ], className="header"),
+        html.Div(
+            children=[
+                html.Div(
+                    children=dcc.Graph(
+                        id="speed-chart",
+                        config={"displayModeBar": False},
+                        figure={
+                            "data": [
+                            {
+                                "x": data["pokedex_number"],
+                                "y": data["speed"],
+                                "type": "lines", # bar
+                            },
+                        ],
+                        "layout": {
+                                "title": {
+                                    "text": "Speed per pokemon",
+                                    "x": 0.05,
+                                    "xanchor": "left",
+                                },
+                                "xaxis": {"fixedrange": True},
+                                "yaxis": {"fixedrange": True},
+                                "colorway": ["purple"],
+                        },
+                        },
+                    ),
+                    className="card",
+                ),
+            ],
+            className="wrapper",
         ),
     ]
 )
